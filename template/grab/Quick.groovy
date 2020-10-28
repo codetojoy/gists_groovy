437a761d78fef90de1af384254dcac8ad9accf5f
Michael Easter <codetojoy@gmail.com>
2020-10-28 13:08:01 -0300

    new template

---
+++ template/grab/Quick.groovy
@@ -0,0 +1,12 @@
+
+@Grab(group='commons-io', module='commons-io', version='2.8.0')
+
+import org.apache.commons.io.FileUtils
+
+// ---------- main 
+
+def file = new File("test.txt")
+
+FileUtils.writeStringToFile(file, "5150\n", "UTF-8", true);
+FileUtils.writeStringToFile(file, "6160\n", "UTF-8", true);
+
