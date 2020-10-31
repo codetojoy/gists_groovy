1edc236c27693bf5f07e022c5039c5354d69c948
Michael Easter <codetojoy@gmail.com>
2020-10-31 10:05:49 -0300

    with @Unroll

--- stack_overflow_64610105/src/test/groovy/net/codetojoy/ExampleSpec.groovy
+++ stack_overflow_64610105/src/test/groovy/net/codetojoy/ExampleSpec.groovy
@@ -4,22 +4,11 @@ package net.codetojoy
 import spock.lang.*
 
 class ExampleSpec extends Specification {
-    def f(x) { x }
-
-    def 'behaviour test for simpleAdd'(int a, int b, int c) {
-     expect:
-      new Example().simpleAdd(a,b) == c
-
-     where:
-       a | b | c
-       5100 | 50 | f(5150) 
-       6160 | 0  | 6160
-    }
-
     @Shared
     def example = new Example()
 
-    def 'behaviour test for experiment'(def name, def route) {
+    @Unroll
+    def 'behaviour test for #name #route'() {
      expect:
       'Hi' == route
 
@@ -27,21 +16,5 @@ class ExampleSpec extends Specification {
        name | route
        'A' | example.doA() 
        'B' | example.doB() 
-    }
-
-    def 'behaviour tests for route A'() {
-     when:
-       def data = new Example().doA();
-
-     then:
-      data == 'Hi'
-    }
-
-    def 'behaviour tests for route B'() {
-     when:
-       def data = new Example().doB();
-
-     then:
-      data == 'Hi'
     }
 }
---
+++ stack_overflow_64610105/src/test/groovy/net/codetojoy/SandboxSpec.groovy
@@ -0,0 +1,47 @@
+
+package net.codetojoy
+
+import spock.lang.*
+
+class SandboxSpec extends Specification {
+    def f(x) { x }
+
+    def 'behaviour test for simpleAdd'(int a, int b, int c) {
+     expect:
+      new Example().simpleAdd(a,b) == c
+
+     where:
+       a | b | c
+       5100 | 50 | f(5150) 
+       6160 | 0  | 6160
+    }
+
+    @Shared
+    def example = new Example()
+
+    def 'behaviour test for experiment'(def name, def route) {
+     expect:
+      'Hi' == route
+
+     where:
+       name | route
+       'A' | example.doA() 
+       'B' | example.doB() 
+    }
+
+    def 'behaviour tests for route A'() {
+     when:
+       def data = new Example().doA();
+
+     then:
+      data == 'Hi'
+    }
+
+    def 'behaviour tests for route B'() {
+     when:
+       def data = new Example().doB();
+
+     then:
+      data == 'Hi'
+    }
+}
