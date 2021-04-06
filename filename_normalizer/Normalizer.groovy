1f3610279a160e15073bb586ced611d557c46b2f
Michael Easter <codetojoy@gmail.com>
2021-04-06 07:51:47 -0300

    incremental update Tue  6 Apr 2021 07:51:47 ADT

--- filename_normalizer/Normalizer.groovy
+++ filename_normalizer/Normalizer.groovy
@@ -4,6 +4,8 @@ def UNDERSCORE = '_'
 def DASH = '-' 
 def SPACE = ' '
 def EMPTY = ''
+def OPEN_PAREN = "("
+def CLOSE_PAREN = ")"
 
 // ----------- main 
 
@@ -11,5 +13,7 @@ def orig = args[0]
 def normal = orig.replace(COMMA, UNDERSCORE)
                  .replace(SPACE, EMPTY)
                  .replace(DASH, UNDERSCORE)
+                 .replace(OPEN_PAREN, UNDERSCORE)
+                 .replace(CLOSE_PAREN, UNDERSCORE)
 
 println normal
