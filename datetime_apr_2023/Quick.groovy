d12d659df67b5ff26f6667e2d9a946c2b7128cbf
Michael Easter <codetojoy@gmail.com>
2023-04-12 20:37:45 -0300

    incremental update Wed 12 Apr 2023 20:37:45 ADT

--- datetime_apr_2023/Quick.groovy
+++ datetime_apr_2023/Quick.groovy
@@ -12,5 +12,14 @@ println dateTime.toString()
 
 def f2 = DateTimeFormatter.ISO_LOCAL_DATE_TIME.withZone(ZoneId.from(ZoneOffset.UTC))
 def now = Instant.now()
+ZonedDateTime zonedDateTime = now.atZone(ZoneId.from(ZoneOffset.UTC))
+def timeStr2 = f1.format(zonedDateTime)
+println "TRACER timeStr2: " + timeStr2
+/*
+println "TRACER yyyy: " + zonedDateTime.get(ChronoField.YEAR)
+println "TRACER mm: " + zonedDateTime.get(ChronoField.MONTH_OF_YEAR)
 def nowStr = f2.format(now)
-println nowStr
+def dateTime2 = f2.format(nowStr)
+println "TRACER datetime2: " + dateTime2.toString()
+*/
+
